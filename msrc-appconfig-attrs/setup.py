79d33e57266d0e959bbdaf62555dc5d2cc29ac7b
Vassily Lyutsarev <vassilyl@users.noreply.github.com>
2025-04-29 13:53:22 +0000

    update metadata

--- msrc-appconfig-attrs/setup.py
+++ msrc-appconfig-attrs/setup.py
@@ -27,11 +27,12 @@ setuptools.setup(
     install_requires=["msrc-appconfig", "attrs"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
--- msrc-appconfig-param/setup.py
+++ msrc-appconfig-param/setup.py
@@ -26,11 +26,12 @@ setuptools.setup(
     install_requires=["msrc-appconfig", "param==1.9.2"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
